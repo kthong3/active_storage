7cf3a01f8add912d746d1933157a9bf1fa8d2c4c
kthong3 <kthongsakounh@gmail.com>
2018-07-18 14:48:58 -0500

    add restful routing in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
 
-  def index
+  def create
+    @post = Post.new(post_params)
+
+    if @post.valid?
+      redirect_to @post
+    else
+     flash[:errors] = @post.errors.full_messages
+     render 'new'
+    end
   end
 
   def show
+  end
+
+  def update
+    @post.update(post_params)
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description, images: [])
   end
 end
